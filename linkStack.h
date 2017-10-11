182e7b8f4d8c07b20a84355da75ebe79dea8bc9f
link <916969890@qq.com>
2017-10-11 08:09:51 +0800

    初始化link stack

---
+++ linkStack.h
@@ -0,0 +1,66 @@
+#define INITSIZE 1
+
+typedef int linkStackET;
+
+typedef struct node{
+    linkStackET val;
+    struct  node *next;
+} node;
+
+typedef struct {
+    node *base;
+    node *top;
+    int length;
+} LinkStack;
+
+//node *createNode(linkStackET val, node *next) {
+
+//}
+
+void linkStackInit(LinkStack *linkStack) {
+    linkStack = malloc(sizeof(LinkStack));
+    linkStack->base = malloc(sizeof(node));
+    if (!linkStack || !linkStack->base) {
+        printf("内存分配失败\n");
+        exit(0);
+    }
+    linkStack->length = 0;
+    linkStack->top = linkStack->base;
+
+}
+
+
+int linkStackPush(LinkStack *linkStack, linkStackET val) {
+    //不存在内存不够用的情况
+
+    node *temp = linkStack->top;
+
+    node* nNode= malloc(sizeof(node));
+    nNode->val = val;
+    nNode->next = NULL;
+
+    //如果是第一个就直接绑定新节点
+    if (linkStack->base == linkStack->top) {
+        temp = nNode;
+    } else {
+        temp->next = nNode;
+    }
+
+    linkStack->top = temp->next;
+    linkStack->length++;
+}
+
+int linkStackLength(LinkStack linkStack) {
+    return linkStack.length;
+}
+
+void linkStackTraverse(LinkStack *linkStack) {
+    if (linkStack->top == linkStack->base) {
+        printf("空栈\n");
+        return;
+    }
+    node *cursor = linkStack->base;
+    do {
+        printf("%d\n", cursor->val);
+    } while(cursor != linkStack->top);
+}
--- main.c
+++ main.c
@@ -3,8 +3,19 @@
 
 #include "common.h"
 
+#include "linkStack.h"
+
 int main() {
-    printf("hello");
+    LinkStack *linkStack;
+    linkStackInit(linkStack);
+    linkStackPush(linkStack, 10);
+
+    linkStackTraverse(linkStack);
+
+    printf("遍历完毕\n");
+    printf("当前栈的长度是: %d\n", linkStackLength(*linkStack));
+
+
 }
 
 
